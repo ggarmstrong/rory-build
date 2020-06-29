2e60e2f02bd050aede6d258783ebd1736a1d6081
Geeves <geeves@Geevess-Mac-mini.local>
2020-06-29 09:05:54 -0700

    first commit

---
+++ precache-manifest.3b3320aee147c59873ad4b6a81a992ac.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a41877cb7c45b3ece63d1d521467b9",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.488bc4b5141083e1370f.bundle.js"
+  },
+  {
+    "url": "runtime~main.488bc4b5141083e1370f.bundle.js"
+  },
+  {
+    "url": "vendors~main.488bc4b5141083e1370f.bundle.js"
+  },
+  {
+    "url": "vendors~main.488bc4b5141083e1370f.bundle.js.LICENSE.txt"
+  }
+]);
